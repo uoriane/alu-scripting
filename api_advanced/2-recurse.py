25bb978b30dfa609683ccf2f361ac58370373ba6
uoriane <o.uwineza@lustudent.com>
2025-07-12 11:17:48 -0700

    api advanced

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,41 +1,43 @@
 #!/usr/bin/python3
-"""
-Recursive function that queries the Reddit API and returns a list of titles
-of all hot articles for a given subreddit. Returns None if subreddit is invalid.
-"""
+"""Recursive function that returns all hot article titles from a subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=None, after=None):
-    """Returns a list of titles of all hot articles for a given subreddit"""
+    """
+    Returns a list of titles of all hot articles for a given subreddit.
+    If subreddit is invalid, returns None.
+    """
     if hot_list is None:
         hot_list = []
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
-        'User-Agent': 'MyRedditScraper/1.0'
+        'User-Agent': 'python:subreddit.hot:v1.0 (by /u/yourusername)'
     }
     params = {'limit': 100}
     if after:
         params['after'] = after
 
-    try:
-        res = requests.get(
-            url, headers=headers, params=params, allow_redirects=False)
-        if res.status_code != 200:
-            return None
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
-        data = res.json().get('data', {})
-        children = data.get('children', [])
-        for child in children:
-            hot_list.append(child.get('data', {}).get('title'))
-
-        next_after = data.get('after')
-        if next_after:
-            return recurse(subreddit, hot_list, next_after)
-
-        return hot_list
-
-    except Exception:
+    # If invalid subreddit or other failure
+    if response.status_code != 200:
         return None
+
+    data = response.json().get("data", {})
+    posts = data.get("children", [])
+
+    for post in posts:
+        title = post.get("data", {}).get("title")
+        if title:
+            hot_list.append(title)
+
+    next_after = data.get("after")
+    if next_after:
+        return recurse(subreddit, hot_list, next_after)
+
+    return hot_list
+
 
